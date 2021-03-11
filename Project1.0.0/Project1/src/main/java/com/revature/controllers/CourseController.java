4d178fd9f5ff7898bcf6bbc37b8aeb82636f3d97
Ben Cady <bjcady14@gmail.com>
2021-03-11 17:26:41 -0600

    changed line 23

--- Project1.0.0/Project1/src/main/java/com/revature/controllers/CourseController.java
+++ Project1.0.0/Project1/src/main/java/com/revature/controllers/CourseController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.models.Course;
 import com.revature.services.CourseService;
 
-import jdk.internal.org.jline.utils.Log;
 
 @RestController
 @RequestMapping("/courses")
